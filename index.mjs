cc846ca8b559ac5d8686841f4dd2d86d88c32991
Hans de Rooij <HansDeRooij@Hotmail.com>
2022-06-18 15:26:54 +0200

    First commit of my cheat sheets

---
+++ index.mjs
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('My cheat cheets hosted on GitHub Codespaces')
+});
+
+app.listen(port, () => {
+  console.log(`Express server listening on port ${port}`)
+});
+
